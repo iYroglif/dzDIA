30290a7ef4729bb68f0a7b54e6771cd9092a918d
iYroglif <webman.com@gmail.com>
2022-09-07 19:52:49 +0300

    startup page done

--- react_frontend/src/components/StartPage.js
+++ react_frontend/src/components/StartPage.js
@@ -1,4 +1,4 @@
-import { Typography, Card, CardMedia, CardContent, Grid } from '@mui/material';
+import { Typography, Card, CardMedia, CardContent, Grid, Box } from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 import courses_dark from '../courses-dark.png';
 import courses_light from '../courses-light.png';
@@ -8,45 +8,26 @@ import student_lab_dark from '../student-lab-dark.png';
 import student_lab_light from '../student-lab-light.png';
 import teacher_student_lab_dark from '../teacher-student-lab-dark.png';
 import teacher_student_lab_light from '../teacher-student-lab-light.png';
+import lab_groups_dark from '../lab-groups-dark.png'
+import lab_groups_light from '../lab-groups-light.png'
 
 export const StartPage = () => {
     const theme = useTheme();
 
     return (
-        <>
-            <Typography gutterBottom variant="h5">Тестовые аккаунты:</Typography>
-            <Grid container spacing={3}>
-                <Grid item>
-                    <Card sx={{ minWidth: "250px" }}>
-                        <CardContent>
-                            <Typography gutterBottom variant="h5">Студент</Typography>
-                            <Typography variant="body2" color="text.secondary">Логин: student</Typography>
-                            <Typography variant="body2" color="text.secondary">Пароль: 5r@M-C_Rx2VhYk_</Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
+        <Grid container direction="column" spacing={3}>
+            <Grid item>
+                <Card sx={{ width: "650px" }}>
+                    <CardMedia sx={{ padding: "1%", width: "98%" }} component="img" image={theme.palette.mode === 'dark' ? courses_dark : courses_light} />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5">Курсы</Typography>
+                        <Typography variant="body2" color="text.secondary">Просматривайте свои текущие курсы</Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
 
-                <Grid item>
-                    <Card sx={{ minWidth: "250px" }}>
-                        <CardContent>
-                            <Typography gutterBottom variant="h5">Преподаватель</Typography>
-                            <Typography variant="body2" color="text.secondary">Логин: teacher</Typography>
-                            <Typography variant="body2" color="text.secondary">Пароль: Y.2Sj6ZN2mczdbb</Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
-
-                <Grid item>
-                    <Card sx={{ width: "650px" }}>
-                        <CardMedia sx={{ padding: "1%", width: "98%" }} component="img" image={theme.palette.mode === 'dark' ? courses_dark : courses_light} />
-                        <CardContent>
-                            <Typography gutterBottom variant="h5">Курсы</Typography>
-                            <Typography variant="body2" color="text.secondary">Просматривайте свои текущие курсы</Typography>
-                        </CardContent>
-                    </Card>
-                </Grid>
-
-                <Grid item>
+            <Grid item>
+                <Box display="flex" justifyContent="flex-end">
                     <Card sx={{ width: "650px" }}>
                         <CardMedia sx={{ padding: "1%", width: "98%" }} component="img" image={theme.palette.mode === 'dark' ? course_labs_dark : course_labs_light} />
                         <CardContent>
@@ -54,8 +35,10 @@ export const StartPage = () => {
                             <Typography variant="body2" color="text.secondary">Просматривайте лабораторные работы своих курсов</Typography>
                         </CardContent>
                     </Card>
-                </Grid>
+                </Box>
+            </Grid>
 
+            <Grid item container justifyContent="space-around" spacing={3}>
                 <Grid item>
                     <Card sx={{ width: "400px" }}>
                         <CardMedia sx={{ padding: "1%", width: "98%" }} component="img" image={theme.palette.mode === 'dark' ? student_lab_dark : student_lab_light} />
@@ -66,16 +49,18 @@ export const StartPage = () => {
                     </Card>
                 </Grid>
 
-                <Grid item>
+                <Grid item alignSelf="end">
                     <Card sx={{ width: "650px" }}>
-                        <CardMedia sx={{ padding: "1%", width: "98%" }} component="img" />
+                        <CardMedia sx={{ padding: "1%", width: "98%" }} component="img" image={theme.palette.mode === 'dark' ? lab_groups_dark : lab_groups_light} />
                         <CardContent>
                             <Typography gutterBottom variant="h5">Лабораторные работы студентов</Typography>
                             <Typography variant="body2" color="text.secondary">Просматривайте лабораторные работы студентов по группам и их статус выполнения</Typography>
                         </CardContent>
                     </Card>
                 </Grid>
+            </Grid>
 
+            <Grid item container justifyContent="space-around" spacing={3}>
                 <Grid item>
                     <Card sx={{ width: "650px" }}>
                         <CardMedia sx={{ padding: "1%", width: "98%" }} component="img" image={theme.palette.mode === 'dark' ? teacher_student_lab_dark : teacher_student_lab_light} />
@@ -86,7 +71,36 @@ export const StartPage = () => {
                     </Card>
                 </Grid>
 
+                <Grid item>
+                    <Grid container direction="column" spacing={2}>
+                        <Grid item>
+                            <Typography gutterBottom variant="h5">Тестовые аккаунты:</Typography>
+                        </Grid>
+
+                        <Grid item container direction="row" spacing={2}>
+                            <Grid item>
+                                <Card>
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5">Студент</Typography>
+                                        <Typography variant="body2" color="text.secondary">Логин: student</Typography>
+                                        <Typography variant="body2" color="text.secondary">Пароль: 5r@M-C_Rx2VhYk_</Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+
+                            <Grid item>
+                                <Card>
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5">Преподаватель</Typography>
+                                        <Typography variant="body2" color="text.secondary">Логин: teacher</Typography>
+                                        <Typography variant="body2" color="text.secondary">Пароль: Y.2Sj6ZN2mczdbb</Typography>
+                                    </CardContent>
+                                </Card>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Grid>
             </Grid>
-        </>
+        </Grid>
     )
 }
