96c0b798f86053e540ea38cc19f228546793eccf
iYroglif <webman.com@gmail.com>
2022-09-07 20:55:19 +0300

    small changes

--- react_frontend/src/components/UserProfile.js
+++ react_frontend/src/components/UserProfile.js
@@ -1,12 +1,13 @@
 import { useContext } from 'react';
 import { UserContext } from '../UserContext';
+import { Typography } from '@mui/material';
 
 export const UserProfile = () => {
     const userContext = useContext(UserContext);
 
     return (
         <>
-            Профиль
+            <Typography>TODO: Профиль</Typography>
         </>
     )
 }
