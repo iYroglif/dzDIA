e1de6a3151be8d38448ac2285dc7f77229db3d64
iYroglif <webman.com@gmail.com>
2022-09-10 22:15:01 +0300

    updates from main

--- django_backend/urls.py
+++ django_backend/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/', include('mysite.urls')),
     path('', TemplateView.as_view(template_name='index.html')),
-    path('/*', TemplateView.as_view(template_name='index.html')),
+    path('*', TemplateView.as_view(template_name='index.html')),
 ]
