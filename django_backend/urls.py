1330906175718c3bb535d53bf5f45604931653b7
iYroglif <webman.com@gmail.com>
2022-09-10 22:23:07 +0300

    try fix url

--- django_backend/urls.py
+++ django_backend/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api/', include('mysite.urls')),
+    path('.*', TemplateView.as_view(template_name='index.html')),
     path('', TemplateView.as_view(template_name='index.html')),
-    path('*', TemplateView.as_view(template_name='index.html')),
 ]
